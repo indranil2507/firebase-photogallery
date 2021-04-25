b1c7e3364cdc018ab7bb14ecf0a3430470842213
indranil2507 <iindranilsaha1@gmail.com>
2021-04-25 20:53:34 +0530

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyADKJPk3bY8WhENJ8i1U-9V11ex3o8h3eY",
+  authDomain: "photo-gallery-6a652.firebaseapp.com",
+  projectId: "photo-gallery-6a652",
+  storageBucket: "photo-gallery-6a652.appspot.com",
+  messagingSenderId: "273016528216",
+  appId: "1:273016528216:web:4a447d017d71d0d757592d",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.staorage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
